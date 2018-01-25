af6832187713bf2c30c72ff9489538d87a08f7f9
Taina Larosiliere <TainaC90@gmail.com>
2018-01-25 17:58:36 -0500

    Subject text field now works

--- public/app2.js
+++ public/app2.js
@@ -5,13 +5,14 @@
 // }
 
 $("#firstAddBtn").click(function(){
-    $("#subjSec").append('<div class="card move" style="width: 8rem;"><div class="card-body"><h5 class="card-title">Subject</h5><input type="text" id="subjName" size="9"> </div></div>');
-    // $("#subjName").submit(); 
+    $("#subjSec").append('<form method= "POST" action = "/newSubject"><div class="card move" style="width: 8rem;"><div class="card-body"><h5 class="card-title">Subject</h5><input type="text" id="subjName" size="9"> </div></div></form>');
+   
 });
 
 $("#secondAddBtn").click(function(){
     $("#cardSec").append('<div class="card move border-success mb-3" style="max-width: 18rem;"><div class="card-header bg-transparent border-success">Index card info: </div><div class="card-body text-success"><h5 class="card-title">Success card title</h5><p class="card-text">Write your study notes here</p></div><div class="card-footer bg-transparent border-success">Resources</div></div>');
 });
+
 
 // window.onload=function() {
 //   document.getElementById("subName").submit(); // using ID
