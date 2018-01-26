f4d7f82dba73a188a55a9424ffa663ad13ba87cf
Taina Larosiliere <TainaC90@gmail.com>
2018-01-26 12:28:27 -0500

    Fixed inputs for cards and decks

--- app.js
+++ app.js
@@ -111,15 +111,6 @@ app.post('/create', url, function (req, res) {
 
     res.redirect('/profile');
 
-//===============================
-    // fileData.push(User);
-    // console.log(fileData.findIndex(x => x.username==username));
-    // fs.writeFile('data.json',JSON.stringify(fileData, null, 2),function(err){
-    //     if(err){
-    //         console.log(err);
-    //     }
-    // });
-    // res.redirect('/profile');
 });
 
 
@@ -178,11 +169,8 @@ app.post('/login',url,function(req,res){
 // ********************************************
 
 var server = app.listen(3000, listening);
-
 function listening(){
-
    console.log("listening")
-
 }
 
 
